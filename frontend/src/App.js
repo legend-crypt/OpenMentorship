19e760ecb0c94a3b5dd261222854c9881e30712e
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2024-01-06 21:30:46 +0530

    fixing merge conflicts after integrating frontend & backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import SignUp from './Pages/SignUp';
 import SignIn from './Pages/SignIn';
@@ -7,15 +6,10 @@ import { Routes, Route } from "react-router-dom";
 import Mentors from './Pages/Mentors';
 import Profile from './Pages/Profile';
 import Verification from './Pages/Verification';
-=======
-import logo from './logo.svg';
-import './App.css';
->>>>>>> py-version-change-to-12
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
     <Routes>
       <Route path="/" element={ <Hero/> } />
       <Route path="/signIn" element={ <SignIn/> } />
@@ -26,22 +20,6 @@ function App() {
       </Route>
     </Routes>
     {/* <Profile/> */}
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> py-version-change-to-12
     </div>
   );
 }
