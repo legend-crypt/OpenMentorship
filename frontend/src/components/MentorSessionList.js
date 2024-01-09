4a18dd2a76ff696a765557cf53754f1233168efc
lord <konadulordkweku@gmai.com>
2024-01-09 04:59:36 +0000

    modified mentor session list component

--- frontend/src/components/MentorSessionList.js
+++ frontend/src/components/MentorSessionList.js
@@ -1,3 +1,19 @@
+/*
+  The `MentorSessionList` React component is a generic component for displaying a list of mentor-mentee session information. It takes a `data` prop containing the session data and a `children` prop representing a callback function to render additional content within each session item.
+
+  Component:
+  - MentorSessionList: Displays a list of mentor-accepted request.
+
+  Key Functionalities:
+  - Maps through the provided `data` array to render individual session items.
+  - Utilizes the `User` component to display mentor information within each session item.
+  - Allows customization of additional content within each session item through the `children` prop.
+
+  Note: This component promotes reusability by providing a flexible structure for displaying mentor-mentee session information with the ability to include custom content.
+*/
+
+
+
 import React from 'react'
 import User from './User'
 
