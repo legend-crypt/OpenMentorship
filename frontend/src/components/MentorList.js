c15d2e430d430d1453def8faee7059b6b9278e8c
lord <konadulordkweku@gmai.com>
2024-01-10 10:06:29 +0000

    fix url error

--- frontend/src/components/MentorList.js
+++ frontend/src/components/MentorList.js
@@ -21,7 +21,7 @@ import useDynamicLogic from '../utils/useDynamicLogic'
 
 export default function MentorList({data}) {
     const {dataList, clickHandler} = useDynamicLogic(
-        'mentors/scheduled-meetings/', "mentor"
+        'mentors/meeting-schedule/', "mentor"
         )
     console.log(`dataList ${dataList}`)
     const mentorList = data.map(item => {
