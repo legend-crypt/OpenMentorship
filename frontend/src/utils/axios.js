236ec0dc941181e9336061e55967178b6c0d8354
lord <konadulordkweku@gmail.com>
2024-05-08 00:16:46 +0000

    Modify axios conf

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -15,7 +15,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/'
+    baseURL: 'https://openmentorship.onrender.com/api/'
 });
 
 export default instance;
