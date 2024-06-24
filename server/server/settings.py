5d8d1384b427a1c5eace7b40b2d9a78fcfbe6ff4
lord <konadulordkweku@gmail.com>
2024-06-24 15:33:23 +0000

    chore: Update database configuration to use environment variable

--- server/server/settings.py
+++ server/server/settings.py
@@ -106,12 +106,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 DATABASES["default"] = dj_database_url.parse(os.getenv("DATABASE_URL"))
 
