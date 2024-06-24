856a216c3804fc4cdbcb6d6550115f906654b631
lord <konadulordkweku@gmail.com>
2024-06-24 15:25:04 +0000

    chore: Add Digital ocean S3 configuration for static and media files storage

--- server/server/settings.py
+++ server/server/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'core',
     'corsheaders',
     'rest_framework_simplejwt',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -149,18 +150,36 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
+# STATIC_URL = 'static/'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STORAGES = {
+#     "default": {
+#         "BACKEND": "django.core.files.storage.FileSystemStorage",
+#     },
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'open-mentors'
+AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL')
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
 }
+AWS_LOCATION_STATIC =  'static/'
+AWS_LOCATION_MEDIA =  'media/'
+
+AWS_DEFAULT_ACL = 'public-read'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'core.storages.MediaStorage'
+STATIC_URL = '{}/{}'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION_STATIC)
+MEDIA_URL = '{}/{}'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION_MEDIA)
+STATIC_ROOT = 'static/'
+Media_ROOT = 'media/'
 
 
 
