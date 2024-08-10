515d4956f768bd1dc16a76b3e644c54ecf0b7ab4
lord <konadulordkweku@gmail.com>
2024-08-10 18:09:25 +0000

    chore: Update axios base URL to use environment variable

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -11,17 +11,13 @@
   Note: This configuration promotes reusability and maintainability by encapsulating the common settings for API requests in a separate file.
 */
 
-
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://openmentorship.onrender.com/api/'
-    // baseURL: 'http://127.0.0.1:8000/api/'
-
+  baseURL: process.env.PRODUCTION_URL,
+  // baseURL: "http://127.0.0.1:8000/api/",
 });
 
 export default instance;
 
-
 // instance for authorized API requests
-
